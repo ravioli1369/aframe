80af5c6aa02cf75b15422a226ac42e4dd26861ba
Ravi Kumar <ravioli1369@gmail.com>
2025-05-28 00:04:21 +0000

    create a function for topological transforms and call it in validation batch creation as well

--- projects/train/train/data/supervised/time_domain.py
+++ projects/train/train/data/supervised/time_domain.py
@@ -6,21 +6,7 @@ from .gwanalyzer import GWAnalyzer
 
 
 class TimeDomainSupervisedAframeDataset(SupervisedAframeDataset):
-    def build_val_batches(self, background, signals):
-        X_bg, X_inj, psds = super().build_val_batches(background, signals)
-        X_bg = self.whitener(X_bg, psds)
-        # whiten each view of injections
-        X_fg = []
-        for inj in X_inj:
-            inj = self.whitener(inj, psds)
-            X_fg.append(inj)
-
-        X_fg = torch.stack(X_fg)
-        return X_bg, X_fg
-
-    def augment(self, X, waveforms):
-        X, y, psds = super().augment(X, waveforms)
-        X = self.whitener(X, psds)
+    def topological_transform(X):
         detector1 = X[:, 0, :]
         detector2 = X[:, 1, :]
         gwana = GWAnalyzer(detector1.cpu().numpy())
@@ -30,4 +16,22 @@ class TimeDomainSupervisedAframeDataset(SupervisedAframeDataset):
         gwana.obtain_topological_features(True, True)
         features2 = torch.tensor(gwana.topological_features, device=X.device)
         X = torch.stack([features1, features2], dim=1)
+        return X
+
+    def build_val_batches(self, background, signals):
+        X_bg, X_inj, psds = super().build_val_batches(background, signals)
+        X_bg = self.whitener(X_bg, psds)
+        X_bg = self.topological_transform(X_bg)
+        # whiten each view of injections
+        X_fg = []
+        for inj in X_inj:
+            inj = self.whitener(inj, psds)
+            inj = self.topological_transform(inj)
+            X_fg.append(inj)
+        X_fg = torch.stack(X_fg)
+        return X_bg, X_fg
+
+    def augment(self, X, waveforms):
+        X, y, psds = super().augment(X, waveforms)
+        X = self.whitener(X, psds)
         return X, y
